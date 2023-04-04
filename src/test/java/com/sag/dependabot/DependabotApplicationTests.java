48cef7698e82ddea79ab5fc42bfcbc1caf9f0ecc
malli <malli@softwareag.com>
2023-04-04 14:52:41 +0530

    init

---
+++ src/test/java/com/sag/dependabot/DependabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sag.dependabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
